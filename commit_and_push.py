6358229510ba7575333938d6dc3ceb96b298b711
seunghyunhwang <hyuny0o0v.01099080116@gmail.com>
2024-08-07 05:30:54 +0900

    test

--- commit_and_push.py
+++ commit_and_push.py
@@ -2,16 +2,13 @@ import os
 import subprocess
 
 def main():
-    # 스테이징된 변경 사항 커밋
     commit_message = "--allow-empty-message --no-edit"
     commit_command = f'git commit {commit_message}'
 
     try:
-        # 변경 사항 커밋
         subprocess.run(commit_command, check=True, shell=True)
         print("Changes committed successfully.")
 
-        # 변경 사항 푸시
         push_command = 'git push origin main'
         subprocess.run(push_command, check=True, shell=True)
         print("Changes pushed to remote repository successfully.")
